ab5232747014af1918072ee3dbaf851d3007a0cb
Yash Johri <yashjohri1200@gmail.com>
2020-09-21 02:02:11 +0530

    Updated links

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,39 +3,40 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "dca820f709f394681b592b12574a87af",
-"assets/assets/images/linkedin.png": "7c2ddc7b8b6595f6d71db4534f0bb098",
-"assets/assets/images/mongodb.png": "8d95890abc0bd2749be918a3be47d4df",
-"assets/assets/images/codechef.png": "2fb8bd067a890e6e6e6c438cc940d629",
+  "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"manifest.json": "87cbfa90b53c626685290282da5c542e",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+"assets/assets/Boy.flr": "e952f57ebe5afe32d4727e31e2e5030e",
+"assets/assets/images/c.png": "1c1888a3eb2f04360763c1a95257b726",
+"assets/assets/images/hackerrank.png": "50538e10603eaa930182af1fbec8978f",
+"assets/assets/images/firebase.png": "ae20121b3839466faef58c1971b8d7e5",
+"assets/assets/images/javascript.png": "2e5de0a7d635b437db088d43f847d39c",
+"assets/assets/images/codeforces.png": "1d395ff8bedd2848730fac2ddcf6333b",
 "assets/assets/images/instagram.png": "bb952b3e5c9b726196de8ee34227edb6",
+"assets/assets/images/mongodb.png": "8d95890abc0bd2749be918a3be47d4df",
+"assets/assets/images/twitter.png": "312082f64163590d6cdf6f2929bf7612",
+"assets/assets/images/linkedin.png": "7c2ddc7b8b6595f6d71db4534f0bb098",
+"assets/assets/images/python.png": "5a53234c474a06637f4176381249534f",
+"assets/assets/images/android.png": "bd2a6f71b226d38ccdbaff124b727080",
 "assets/assets/images/nodejs.png": "b73453c62371ffa61b16dce4dd72ae65",
 "assets/assets/images/sql.png": "7a76cff597d8b80b3f1dc36a28bdd39d",
-"assets/assets/images/dart.png": "9271c5feaa7176d4bc87467c11eaa8c0",
-"assets/assets/images/c.png": "1c1888a3eb2f04360763c1a95257b726",
-"assets/assets/images/github.png": "9bd526d73178fa9d0331801a0b6ba056",
-"assets/assets/images/hackerrank.png": "50538e10603eaa930182af1fbec8978f",
-"assets/assets/images/android.png": "bd2a6f71b226d38ccdbaff124b727080",
 "assets/assets/images/git.png": "cca1d72813c340797640c2975925a315",
-"assets/assets/images/twitter.png": "312082f64163590d6cdf6f2929bf7612",
-"assets/assets/images/flutter.png": "67642a0b80f3d50277c44cde8f450e50",
-"assets/assets/images/javascript.png": "2e5de0a7d635b437db088d43f847d39c",
-"assets/assets/images/firebase.png": "ae20121b3839466faef58c1971b8d7e5",
-"assets/assets/images/cpp.png": "fbc85d8dbc00b63fab032f19dba650d2",
-"assets/assets/images/python.png": "5a53234c474a06637f4176381249534f",
-"assets/assets/images/codeforces.png": "1d395ff8bedd2848730fac2ddcf6333b",
+"assets/assets/images/codechef.png": "2fb8bd067a890e6e6e6c438cc940d629",
 "assets/assets/images/java.png": "458aa8023c52e48ce5b87d0c764fb414",
-"assets/assets/Boy.flr": "e952f57ebe5afe32d4727e31e2e5030e",
+"assets/assets/images/dart.png": "9271c5feaa7176d4bc87467c11eaa8c0",
+"assets/assets/images/flutter.png": "67642a0b80f3d50277c44cde8f450e50",
+"assets/assets/images/cpp.png": "fbc85d8dbc00b63fab032f19dba650d2",
+"assets/assets/images/github.png": "9bd526d73178fa9d0331801a0b6ba056",
 "assets/fonts/MaterialIcons-Regular.otf": "a68d2a28c526b3b070aefca4bac93d25",
-"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/AssetManifest.json": "a19df4e1347ff310a121a74ef6ad5daf",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/NOTICES": "7fcbf15afb867b561dd7d8e5934610b5",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"manifest.json": "87cbfa90b53c626685290282da5c542e",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"index.html": "97a4f5ca11dd822f83bb91dd2e7f65d3",
-"/": "97a4f5ca11dd822f83bb91dd2e7f65d3"
+"assets/AssetManifest.json": "a19df4e1347ff310a121a74ef6ad5daf",
+"assets/NOTICES": "38df9286bad83c479351cc4c765eef43",
+"version.json": "408798af188378b64d27faac697a675e",
+"main.dart.js": "b471e33751be7bf5fa293cbe1ecf39bc",
+"index.html": "8da193eb949052eba0238a00984dede0",
+"/": "8da193eb949052eba0238a00984dede0"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -49,6 +50,7 @@ const CORE = [
 "assets/FontManifest.json"];
 // During install, the TEMP cache is populated with the application shell files.
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
@@ -117,6 +119,9 @@ self.addEventListener("activate", function(event) {
 // The fetch handler redirects requests for RESOURCE files to the service
 // worker cache.
 self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
   var origin = self.location.origin;
   var key = event.request.url.substring(origin.length + 1);
   // Redirect URLs to the index.html
@@ -126,9 +131,10 @@ self.addEventListener("fetch", (event) => {
   if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
     key = '/';
   }
-  // If the URL is not the RESOURCE list, skip the cache.
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
   if (!RESOURCES[key]) {
-    return event.respondWith(fetch(event.request));
+    return;
   }
   // If the URL is the index.html, perform an online-first request.
   if (key == '/') {
@@ -152,10 +158,12 @@ self.addEventListener('message', (event) => {
   // SkipWaiting can be used to immediately activate a waiting service worker.
   // This will also require a page refresh triggered by the main worker.
   if (event.data === 'skipWaiting') {
-    return self.skipWaiting();
+    self.skipWaiting();
+    return;
   }
-  if (event.message === 'downloadOffline') {
+  if (event.data === 'downloadOffline') {
     downloadOffline();
+    return;
   }
 });
 
